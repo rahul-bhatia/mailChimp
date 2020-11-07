631450a1026f2416b229378b02f67e6a0c38a3ab
Rahul <rahul.1517.bhatia@gmail.com>
2020-11-07 15:03:07 +0530

    favicon disabled

--- app.js
+++ app.js
@@ -12,9 +12,17 @@ app.get("/",function(req,res){
 });
 
 app.post("/",function(req,res){
-  console.log(req.body);
+  const firstName=req.body.fName;
+  const lastName=req.body.lName;
+  const email=req.body.inputEmail;
+
+
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log("App listening at port 3000");
 });
+
+
+
+//api key -- 24fefc58bee1c543eb6619a390e4294c-us2
